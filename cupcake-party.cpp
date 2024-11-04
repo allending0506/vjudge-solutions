c938ebc532ff99f832ac6225cac45051dc7e114d
allending0506 <157081702+allending0506@users.noreply.github.com>
2024-11-03 17:09:30 -0800

    Create cupcake-party.cpp

---
+++ cupcake-party.cpp
@@ -0,0 +1,12 @@
+//CCC '22 J1 - Cupcake party
+//https://dmoj.ca/problem/ccc22j1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int r, s;
+  cin >> r >> s;
+  cout << r * 8 + s * 3 - 28;
+  return 0;
+}
