d0b843f1b54bc69bfe2a9b47f7712777147af6c6
allending0506 <157081702+allending0506@users.noreply.github.com>
2024-11-12 08:29:27 -0800

    Create whoistaller.cpp

---
+++ ccc/whoistaller.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int taller[1000000][2] {-1};
+vector<int> tallert[100000];
+
+void findPath(int p, int q, int n, int m){
+  queue<int> visitNextTaller;
+  queue<int> visitNextShorter;
+  bool visitedTaller[100000] {false};
+  bool visitedShorter[100000] {false};
+  visitNextTaller.push(p);
+  visitNextShorter.push(q);
+  while (!visitNextTaller.empty() || !visitNextShorter.empty()){
+    if (!visitNextTaller.empty()){
+      visitedTaller[visitNextTaller.front()] = true;
+      if (visitNextTaller.front() == q){
+        cout << "yes";
+        return;
+      }
+      for (auto& i : tallert[visitNextTaller.front()]){
+        if(!visitedTaller[i]){
+          visitNextTaller.push(i);
+          visitedTaller[i] = true;
+        }
+      }
+      visitNextTaller.pop();
+    }
+    if (!visitNextShorter.empty()){
+      visitedShorter[visitNextShorter.front()] = true;
+      if (visitNextShorter.front() == p){
+        cout << "no";
+        return;
+      }
+      for (auto& i : tallert[visitNextShorter.front()]){
+        if(!visitedShorter[i]){
+          visitNextShorter.push(i);
+          visitedShorter[i] = true;
+        }
+      }
+      visitNextShorter.pop();
+    }
+  }
+  cout << "unknown";
+}
+
+int main(){
+  int n, m, p, q;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++){
+    int tem1, tem2;
+    cin >> tem1 >> tem2;
+    tallert[tem1].push_back(tem2);
+  }
+  cin >> p >> q;
+  findPath(p, q, n, m);
+  return 0;
+}
