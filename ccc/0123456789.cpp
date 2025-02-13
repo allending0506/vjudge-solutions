a34b66faa414ecfbddf1be41d9007b9ca4a0a0ca
allending0506 <157081702+allending0506@users.noreply.github.com>
2025-02-13 11:01:09 -0800

    Create 0123456789.cpp

---
+++ ccc/0123456789.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  switch (n){
+    case 0:
+      cout << " * * *\n*     *\n*     *\n*     *\n\n*     *\n*     *\n*     *\n * * *\n";
+      break;
+    case 1:
+      cout << "\n      *\n      *\n      *\n\n      *\n      *\n      *\n\n";
+      break;
+    case 2:
+      cout << " * * *\n      *\n      *\n      *\n * * *\n*\n*\n*\n * * *\n";
+      break;
+    case 3:
+      cout << " * * *\n      *\n      *\n      *\n * * *\n      *\n      *\n      *\n * * *\n";
+      break;
+    case 4:
+      cout << "\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n\n";
+      break;
+    case 5:
+      cout << " * * *\n*\n*\n*\n * * *\n      *\n      *\n      *\n * * *\n";
+      break;
+    case 6:
+      cout << " * * *\n*\n*\n*\n * * *\n*     *\n*     *\n*     *\n * * *\n";
+      break;
+    case 7:
+      cout << " * * *\n      *\n      *\n      *\n\n      *\n      *\n      *\n\n";
+      break;
+    case 8:
+      cout << " * * *\n*     *\n*     *\n*     *\n * * *\n*     *\n*     *\n*     *\n * * *\n";
+      break;
+    case 9:
+      cout << " * * *\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n * * *\n";
+  }
+  return 0;
+}
